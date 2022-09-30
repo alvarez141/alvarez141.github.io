8cf4d6505b0c45a5c27c19169e729a4526c45939
alvarez141 <alvarezdanieldelgadillo@gmail.com>
2022-09-30 14:15:43 +0200

    https

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,35 +1,34 @@
 // import React, {useEffect, useState} from "react";
-import React from 'react';
-import ProductItem from '@components/ProductItem';
-import '@styles/ProductList.scss';
+import React from "react";
+import ProductItem from "@components/ProductItem";
+import "@styles/ProductList.scss";
 // import axios from 'axios';
-import useGetProducts from '@hooks/useGetProducts';
+import useGetProducts from "@hooks/useGetProducts";
 
-const API ='https://api.escuelajs.co/api/v1/products';
-
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	// const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
 
-	// useEffect(() => {
-	// 	getProducts()
-	// }, [])
+  // useEffect(() => {
+  // 	getProducts()
+  // }, [])
 
-	// const getProducts = async () =>{
-	// 	const response = await axios(API);
-	// 	setProducts(response.data);
-	// };
-	const products = useGetProducts(API); 
-	return (
-        <section className="main-container">
-			<div className="ProductList">
-				{/* revuelve todos los prodcutos de una API */}
-			{products.map(product => (
-				<ProductItem product={product} key={product.id} />
-			))}
-			</div>
-		</section>
-    );
-}
+  // const getProducts = async () =>{
+  // 	const response = await axios(API);
+  // 	setProducts(response.data);
+  // };
+  const products = useGetProducts(API);
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {/* revuelve todos los prodcutos de una API */}
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
