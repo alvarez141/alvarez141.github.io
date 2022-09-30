84a3a9c0c30c508fb475e9b4fe655ee079947971
alvarez141 <alvarezdanieldelgadillo@gmail.com>
2022-09-30 12:31:49 +0200

    [APP]

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,63 +1,57 @@
 //modulo
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //Moleculas(components)
-import Order from '../components/Order';
-
+import Order from "../components/Order";
 
 //Organismos(containers)
-import Layout from '../containers/Layout';
-import Login from '../pages/Login';
-import SendEmail from '../pages/SendEmail';
-
+import Layout from "../containers/Layout";
+import Login from "../pages/Login";
+import SendEmail from "../pages/SendEmail";
 
 //paginas(page)
-import Home from '../pages/Home';
-import NotFount from '../pages/NotFound';
-import NewPassword from '../pages/NewPassword';
-import CreateAccount from '../pages/CreateAccount';
-import Orders from '../pages/Orders';
-import MyAccount from '../pages/MyAccount';
-import Checkout from '../pages/Checkout';
-import RecoveryPassword from '../pages/PasswordRecovery';
+import Home from "../pages/Home";
+import NotFount from "../pages/NotFound";
+import NewPassword from "../pages/NewPassword";
+import CreateAccount from "../pages/CreateAccount";
+import Orders from "../pages/Orders";
+import MyAccount from "../pages/MyAccount";
+import Checkout from "../pages/Checkout";
+import RecoveryPassword from "../pages/PasswordRecovery";
 
 //context
-import AppContext from '@context/AppContext';
-
-
+import AppContext from "@context/AppContext";
 
 //style
-import '../styles/global.css';
+import "../styles/global.css";
 
 //hooks
-import useInitialState from '../hooks/useInitialState';
-
-
+import useInitialState from "../hooks/useInitialState";
 
 function App() {
-    const initialState = useInitialState();
-    return (
-        <AppContext.Provider value={initialState}>
-            <BrowserRouter>
-                <Layout>
-                    <Routes>
-                        <Route path='/' element={<Home />}/>
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/send-email' element={<SendEmail />} />
-                        <Route path='/create-account' element={<CreateAccount />} />
-                        <Route path='new-password' element={<NewPassword />} />
-                        <Route path='/order' element={<Order />} />
-                        <Route path='/orders' element={<Orders />} />
-                        <Route path='/my-account' element={<MyAccount />} />
-                        <Route path='/checkout' element={<Checkout />} />
-                        <Route path='/recovery-password' element={<RecoveryPassword />} />
-                        <Route path='*' element={<NotFount />}/>
-                    </Routes>
-                </Layout>
-            </BrowserRouter>
-        </AppContext .Provider>
-    );
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/send-email" element={<SendEmail />} />
+            <Route path="/create-account" element={<CreateAccount />} />
+            <Route path="new-password" element={<NewPassword />} />
+            <Route path="/order" element={<Order />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/my-account" element={<MyAccount />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/recovery-password" element={<RecoveryPassword />} />
+            <Route path="*" element={<NotFount />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,86 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-		publicPath: '/',
-	},
-	mode: 'development',
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components/'),
-			'@containers': path.resolve(__dirname, 'src/containers/'),
-			'@pages': path.resolve(__dirname, 'src/pages/'),
-      		'@styles': path.resolve(__dirname, 'src/styles/'),
-			'@icons': path.resolve(__dirname, 'public/assets/icons/'),
-			'@logos': path.resolve(__dirname, 'public/assets/logos/'),
-			'@routes': path.resolve(__dirname, 'src/routes/'),
-			'@hooks': path.resolve(__dirname, 'src/hooks/'),
-			'@context': path.resolve(__dirname, 'src/context/'),
-
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader'
-					}
-				]
-			},
-			{
-				test: /\.(css|s[ac]ss)$/i,
-				use: [
-					// Crea nodos `style` a partir de cadenas JS
-					"style-loader",
-					// Traduce el CSS a CommonJS
-					"css-loader",
-					// Compila Sass a CSS
-					"sass-loader",
-				],
-			},
-			{	//carga las imagenes
-				//npm install --save-dev file-loader
-				test: /\.(png|jpg|svg|jp(e*)g|webp|gif)$/i,
-				use: [
-					{
-					  loader: 'file-loader',
-					  options: { name: 'assets/pictures/[hash].[ext]' },
-					},
-				],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html',
-			filename: './index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		}),
-	],
-	devServer: {
-		static: {
-			// lo puedes llamar public o dist
-		  directory: path.join(__dirname, 'dist'),
-		},
-		historyApiFallback: true,
-		compress: true,
-		port: 9000,
-	},
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "./",
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@containers": path.resolve(__dirname, "src/containers/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@icons": path.resolve(__dirname, "public/assets/icons/"),
+      "@logos": path.resolve(__dirname, "public/assets/logos/"),
+      "@routes": path.resolve(__dirname, "src/routes/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
+      "@context": path.resolve(__dirname, "src/context/"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [
+          // Crea nodos `style` a partir de cadenas JS
+          "style-loader",
+          // Traduce el CSS a CommonJS
+          "css-loader",
+          // Compila Sass a CSS
+          "sass-loader",
+        ],
+      },
+      {
+        //carga las imagenes
+        //npm install --save-dev file-loader
+        test: /\.(png|jpg|svg|jp(e*)g|webp|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "assets/pictures/[hash].[ext]" },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+  ],
+  devServer: {
+    static: {
+      // lo puedes llamar public o dist
+      directory: path.join(__dirname, "dist"),
+    },
+    historyApiFallback: true,
+    compress: true,
+    port: 9000,
+  },
+};
